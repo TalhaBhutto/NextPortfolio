2254ecce55f322f0abbbb7453bc1e475de1d3417
Talha Hussain Bhutto <talhabhutto420@outlook.com>
2021-09-07 15:53:03 +0500

    discription edit

--- my_portfolio/components/Projects/Projects.js
+++ my_portfolio/components/Projects/Projects.js
@@ -29,7 +29,7 @@ function Projects() {
         {projects.map((p, i) => {
           return (
             <BlogCard key={i}>
-              <Img src={p.image} />
+              {/* <Img src={p.image} /> */}
               <TitleContent>
                 <HeaderThree title>{p.title}</HeaderThree>
                 <Hr />
