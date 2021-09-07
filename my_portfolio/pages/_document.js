5a30e3ec28b2e67dcfdee246dc0e3e6dfe718eaa
Talha Hussain Bhutto <talhabhutto420@outlook.com>
2021-09-07 15:14:13 +0500

    added display name

--- my_portfolio/pages/_document.js
+++ my_portfolio/pages/_document.js
@@ -2,7 +2,6 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  displayName = "MyDocument";
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -10,6 +9,7 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
+          displayName: "MyDocument",
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
